b458b5fa4f20d7680267396c447266312eb4a32c
Mo <mohamed.sayed0430@gmail.com>
2024-09-09 20:27:27 +0300

    var

--- variable.tf
+++ variable.tf
@@ -1,11 +1,6 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
 variable "vpc_name" {
   type    = string
-  default = "demo_vpc"
+  default = "demo_vpc_HA"
 }
 
 variable "vpc_cidr" {
@@ -17,14 +12,11 @@ variable "private_subnets" {
   default = {
     "private_subnet_1" = 1
     "private_subnet_2" = 2
-    "private_subnet_3" = 3
   }
 }
 
 variable "public_subnets" {
   default = {
     "public_subnet_1" = 1
-    "public_subnet_2" = 2
-    "public_subnet_3" = 3
   }
 }
