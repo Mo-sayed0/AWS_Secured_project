d56293c4a4328a38ad847db0585729d34d120e63
Mo <mohamed.sayed0430@gmail.com>
2024-09-10 00:59:12 +0300

    variable

--- variable.tf
+++ variable.tf
@@ -41,24 +41,3 @@ variable "availability_zone" {
     "availability_zone_1f" = "us-east-1f"
   }
 }
-
-variable "public_subnets_rtb" {
-  type = map(object({
-    id = string
-  }))
-  default = {
-    public_subnet_1 = { id = "aws_subnet.public_subnet_1.id" },
-    public_subnet_2 = { id = "aws_subnet.public_subnet_2.id" }
-  }
-}
-variable "private_subnets_rtb" {
-  type = map(object({
-    id = string
-  }))
-  default = {
-    private_subnet_1 = { id = "aws_subnet.private_subnet_1.id" },
-    private_subnet_3 = { id = "aws_subnet.private_subnet_3.id" },
-    private_subnet_2 = { id = "aws_subnet.private_subnet_2.id" },
-    private_subnet_4 = { id = "aws_subnet.private_subnet_4.id" }
-  }
-}
