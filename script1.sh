abb4fa089702062534e2871f0ff5bdd9576089e8
Mohamed Sayed <53582383+Mo-sayed0@users.noreply.github.com>
2024-09-15 15:41:15 +0300

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Update the system packages
+sudo yum update -y
+
+# Install Apache HTTP Server
+sudo yum install -y httpd
+
+# Capture hostname and IP address and write to the index.html file
+echo "This is $(hostname) and the IP is $(hostname -I)" > /var/www/html/index.html
+
+# Enable and start Apache HTTP Server
+systemctl enable httpd
+systemctl start httpd
