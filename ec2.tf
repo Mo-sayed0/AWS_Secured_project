7da7689963ba67c42ae8f9e246abe14168c2ec60
Mo <mohamed.sayed0430@gmail.com>
2024-09-16 10:22:23 +0200

    rds

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,15 @@
-resource "aws_key_pair" "ssh_key" {
-  key_name = "memo_key"
-  public_key = file("~/.ssh/memo.pub")
-}
-
 
 resource "aws_instance" "web_ec2" {
-  count                   = var.public_subnet_count
+  count         = var.public_subnet_count
   ami           = data.aws_ami.image.id
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnets[count.index].id
-  key_name = "memo_key"
+  subnet_id     = aws_subnet.public_subnets[count.index].id
   connection {
     type = "ssh"
     user = "ec2-user"
-    private_key = ("~/.ssh/memo")
     host = self.private_ip
   }
-  user_data = filebase64("${path.module}/script1.sh")
+  user_data              = filebase64("${path.module}/script1.sh")
   vpc_security_group_ids = [aws_security_group.sg_web.id]
 
   tags = {
@@ -25,20 +18,17 @@ resource "aws_instance" "web_ec2" {
 }
 
 resource "aws_instance" "app_ec2" {
-  count = var.private_subnet_count >= 2 ? 2 : 0
+  count         = var.private_subnet_count >= 2 ? 2 : 0
   ami           = data.aws_ami.image.id
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.private_subnets[count.index].id
-  key_name = "memo_key"
+  subnet_id     = aws_subnet.private_subnets[count.index].id
   connection {
     type = "ssh"
     user = "ec2-user"
-    private_key = ("~/.ssh/memo")
     host = self.private_ip
   }
-  user_data = filebase64("${path.module}/script1.sh")
+  user_data              = filebase64("${path.module}/script1.sh")
   vpc_security_group_ids = [aws_security_group.sg_app.id]
-
   tags = {
     Name = "HA-app-${count.index}"
   }
--- rds.tf
+++ rds.tf
@@ -1,1 +1,48 @@
+# Random string for DB password
+resource "random_password" "db_password" {
+  length  = 16
+  special = false
+}
 
+# RDS Subnet Group
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [for subnet in aws_subnet.private_subnets : subnet.id]
+  tags = {
+    Name = "RDS Subnet Group"
+  }
+}
+
+# RDS Instance
+resource "aws_db_instance" "mysql_rds" {
+  count                  = var.private_subnet_count >= 3 ? 1 : 0
+  identifier             = "myapp-rds-instance"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  db_name                = "myappdb"
+  username               = "admin"
+  password               = random_password.db_password.result
+  parameter_group_name   = "default.mysql8.0"
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.id
+  vpc_security_group_ids = [aws_security_group.sg_db.id]
+  multi_az               = true
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  tags = {
+    Name = "MyApp-RDS-Instance"
+  }
+}
+
+# Output the RDS endpoint
+output "rds_endpoint" {
+  value = var.private_subnet_count >= 3 ? aws_db_instance.mysql_rds[0].endpoint : "Not enough private subnets for RDS"
+}
+
+# Output the RDS password (Be cautious with this in production environments)
+output "rds_password" {
+  value     = random_password.db_password.result
+  sensitive = true
+}
