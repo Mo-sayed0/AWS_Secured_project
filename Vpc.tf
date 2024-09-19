7f6f3f0e717c9c3cd1cf20bde6522f1fa53f1477
Mohamed Sayed <53582383+Mo-sayed0@users.noreply.github.com>
2024-09-19 12:40:20 +0300

    Update Vpc.tf

--- Vpc.tf
+++ Vpc.tf
@@ -1,7 +1,7 @@
-#Define the VPC
+
+# VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
-
   tags = {
     Name        = var.vpc_name
     Environment = "demo_environment"
@@ -9,89 +9,91 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Deploy the private subnets
+# Subnets
 resource "aws_subnet" "private_subnets" {
   count             = var.private_subnet_count
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 8)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 6)
   availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
-
   tags = {
     Name      = "Private_Subnet_${count.index + 1}"
     Terraform = "true"
   }
 }
 
-# Deploy the public subnets
-resource "aws_subnet" "public_subnets" {
-  count                   = var.public_subnet_count
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index + 5)
-  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
-  map_public_ip_on_launch = true
-
+resource "aws_subnet" "private_subnets_db" {
+  count             = var.private_subnet_count_db
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + var.private_subnet_count)
+  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
   tags = {
-    Name      = "public_Subnet_${count.index + 1}"
+    Name      = "Private_Subnet_db_${count.index + 1}"
     Terraform = "true"
   }
 }
 
-#Create Internet Gateway
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_subnet" "public_subnets" {
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index + var.private_subnet_count + var.private_subnet_count_db)
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  map_public_ip_on_launch = true
   tags = {
-    Name = "igw_HA_3-Tire"
+    Name      = "Public_Subnet_${count.index + 1}"
+    Terraform = "true"
   }
 }
 
-#Create EIP for NAT Gateway
+# Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "igw_HA_3-Tier"
+  }
+}
+
+# NAT Gateway
 resource "aws_eip" "nat_gateway_eip" {
   domain     = "vpc"
   depends_on = [aws_internet_gateway.internet_gateway]
   tags = {
-    Name = "igw_eip_HA_3-Tire"
+    Name = "igw_eip_HA_3-Tier"
   }
 }
 
-#Create NAT Gateway
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id     = aws_subnet.public_subnets[0].id
   tags = {
-    Name = "nat_gateway_HA_3-Tire"
+    Name = "nat_gateway_HA_3-Tier"
   }
 }
 
-#Create route tables for public and private subnets
+# Route Tables
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
-    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
-    Name      = "public_rtb_HA_3-Tire"
+    Name      = "public_rtb_HA_3-Tier"
     Terraform = "true"
   }
 }
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
-
   route {
-    cidr_block = "0.0.0.0/0"
-    #gateway_id     = aws_internet_gateway.internet_gateway.id
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
-    Name      = "Private_rtb_HA_3-Tire"
+    Name      = "Private_rtb_HA_3-Tier"
     Terraform = "true"
   }
 }
 
-# Create route table associations
 resource "aws_route_table_association" "public" {
   count          = var.public_subnet_count
   route_table_id = aws_route_table.public_route_table.id
@@ -102,4 +104,10 @@ resource "aws_route_table_association" "private" {
   count          = var.private_subnet_count
   route_table_id = aws_route_table.private_route_table.id
   subnet_id      = aws_subnet.private_subnets[count.index].id
+}
+
+resource "aws_route_table_association" "private_db" {
+  count          = var.private_subnet_count_db
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnets_db[count.index].id
 }
