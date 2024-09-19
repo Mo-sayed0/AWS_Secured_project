11373683ac07ee53a5eb5fb11cbbf78b57e1665d
Mohamed Sayed <53582383+Mo-sayed0@users.noreply.github.com>
2024-09-19 12:44:15 +0300

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,85 +1,89 @@
+
+# Security Groups
 resource "aws_security_group" "sg_web" {
-  name                = "webserver"
-  description         = "Security Group for Web Servers"
+  name        = "webserver"
+  description = "Security Group for Web Servers"
   vpc_id      = aws_vpc.vpc.id
+
   dynamic "ingress" {
-    for_each = local.ingree_web
+    for_each = [80, 443]
     content {
-        from_port        = ingress.value
-        to_port          = ingress.value
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }   
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   dynamic "egress" {
     for_each = local.egress_web
     content {
-        from_port        = egress.value
-        to_port          = egress.value
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }   
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
+
   tags = {
     Name = "HA_sg-web"
   }
 }
 
 resource "aws_security_group" "sg_app" {
-  name = "appserver"
+  name   = "appserver"
+  vpc_id = aws_vpc.vpc.id
 
-  vpc_id      = aws_vpc.vpc.id
-  
   dynamic "ingress" {
-      for_each = local.ingree_app
-      content {
-          from_port        = ingress.value
-          to_port          = ingress.value
-          protocol         = "tcp"
-          security_groups = [aws_security_group.sg_web.id]
-      }   
+    for_each = local.egress_app
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = [aws_security_group.sg_web.id]
     }
+  }
 
-    dynamic "egress" {
-      for_each = local.egress_app
-      content {
-          from_port        = egress.value
-          to_port          = egress.value
-          protocol         = "-1"
-          cidr_blocks      = ["0.0.0.0/0"]
-      }
+  dynamic "egress" {
+    for_each = local.egress_app
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
     }
+  }
+
   tags = {
     Name = "HA_sg-app"
-  }  
+  }
 }
 
 resource "aws_security_group" "sg_db" {
-  name = "database"
+  name   = "database"
+  vpc_id = aws_vpc.vpc.id
 
-  vpc_id      = aws_vpc.vpc.id
   dynamic "ingress" {
-    for_each = local.ingree_db
+    for_each = local.egress_db
     content {
-        from_port        = ingress.value
-        to_port          = ingress.value
-        protocol         = "tcp"
-        security_groups = [aws_security_group.sg_web.id]
-    }   
-}
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = [aws_security_group.sg_app.id]
+    }
+  }
 
-	dynamic "egress" {
-		for_each = local.egress_db
-		content {
-				from_port        = egress.value
-				to_port          = egress.value
-				protocol         = "-1"
-				cidr_blocks      = ["0.0.0.0/0"]
-			
-			}
-	}
-	tags = {
-			Name = "HA_sg-db"
-	}   
+  dynamic "egress" {
+    for_each = local.egress_db
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "HA_sg-db"
+  }
 }
