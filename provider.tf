930990c167c7801060ddedfe29750c8a2a117fc4
Mohamed Sayed <53582383+Mo-sayed0@users.noreply.github.com>
2024-09-23 10:19:36 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,3 @@ provider "aws" {
   region  = "us-east-1"
   profile = "terraform-dev"
 }
-resource "aws_key_pair" "my_key_pair" {
-  key_name   = "Terraform-Project"
-  public_key = file("~/.ssh/my_aws_key.pub")
-}
